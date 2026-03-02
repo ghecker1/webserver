156fce289b6929fcfbe6deba56ddc95a1d22b50b
Gordon Hecker <gordon@ghecker.net>
2026-03-02 17:14:15 +0100

    fix

--- webserver/webserver.ino
+++ webserver/webserver.ino
@@ -14,6 +14,7 @@ WiFiClient *client;
 
 unsigned long connect_time = 0;
 unsigned long l = 0;
+int headers_size = 0;
 char headers[2048];
 char content[2048];
 
@@ -22,7 +23,8 @@ void printScanResult(int networksFound)
   Serial.printf("%d network(s) found\n", networksFound);
   for (int i = 0; i < networksFound; i++)
   {
-    Serial.printf("%d: %s, Ch:%d (%ddBm) %s\n", i + 1, WiFi.SSID(i).c_str(), WiFi.channel(i), WiFi.RSSI(i), WiFi.encryptionType(i) == ENC_TYPE_NONE ? "open" : "");
+    Serial.printf("%d: %s, Ch:%d (%ddBm) %s", i + 1, WiFi.SSID(i).c_str(), WiFi.channel(i), WiFi.RSSI(i), WiFi.encryptionType(i) == ENC_TYPE_NONE ? "open" : "");
+    Serial.println();
   }
 }
 
@@ -53,21 +55,26 @@ void setup() {
   // Print the IP address
   Serial.println(WiFi.localIP());
   delay(500);
+
+  headers_size = sprintf(headers, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: close\r\nContent-length: 102400\r\n\r\n");
 }
 
 int serveClient() {
   static unsigned int totalWritten = 0;
   static unsigned int total = 100 * 1024;
   static uint8_t status = 0;
-  static char headers[] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: close\r\nContent-length: 102400\r\n\r\n";
 
   //Serial.println(F("Entering serveClient()"));
   int n = client->availableForWrite();
-  if (status == 0 && n > strlen(headers)) {
-    Serial.println("Send headers");
-    client->write(headers, strlen(headers));
-    memset(content, 'a', sizeof(content));
-    status = 1;
+  if (status == 0)
+    if (n > headers_size) {
+      Serial.println("Send headers");
+      client->write(headers, headers_size);
+      memset(content, 'a', sizeof(content));
+      status = 1;
+    } else {
+      Serial.println("Waiting");
+    }
   } else {
     if (totalWritten + n > total) {
       n = total - totalWritten;
