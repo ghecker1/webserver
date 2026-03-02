2dcfb2ae1535bd455b0716462e08513059617a6c
Gordon Hecker <gordon@ghecker.net>
2026-03-02 19:54:05 +0100

    fix

--- webserver/webserver.ino
+++ webserver/webserver.ino
@@ -12,11 +12,6 @@ char pass[] = "pppppppppppppppppppp";
 WiFiServer server(80);
 WiFiClient *client;
 
-unsigned long connect_time = 0;
-unsigned long l = 0;
-int headers_size = 0;
-char headers[2048];
-char content[2048];
 
 void printScanResult(int networksFound)
 {
@@ -54,32 +49,40 @@ void setup() {
 
   // Print the IP address
   Serial.println(WiFi.localIP());
-  delay(500);
-
-  headers_size = sprintf(headers, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: close\r\nContent-length: 102400\r\n\r\n");
 }
+
+unsigned long connect_time = 0;
+unsigned long l = 0;
+int headers_size = 0;
+char headers[512];
+char content[2048];
+uint8_t status = 0;
 
 int serveClient() {
   static unsigned int totalWritten = 0;
   static unsigned int total = 100 * 1024;
-  static uint8_t status = 0;
 
   //Serial.println(F("Entering serveClient()"));
   int n = client->availableForWrite();
   if (status == 0) {
+    connect_time = millis();
+    headers_size = sprintf(headers, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: close\r\nContent-length: %d\r\n\r\n", total);
+    memset(content, 'a', sizeof(content));
     if (n > headers_size) {
       Serial.print("Send headers: ");
       Serial.printf("%d", headers_size);
       Serial.println();
       client->write(headers, headers_size);
-      memset(content, 'a', sizeof(content));
       status = 1;
     } else {
-      Serial.println("Waiting");
+      Serial.println("Cannot send headers...");
     }
-  } else if (n > 0) {
+  } else if (status == 1 && n > 0) {
     if (totalWritten + n > total) {
       n = total - totalWritten;
+    }
+    if (n > sizeof(content)) {
+      n = sizeof(content);
     }
     client->write(content, n);
     totalWritten += n;
@@ -103,23 +106,20 @@ void loop() {
   if (!client && server.hasClient()) {
     client = new WiFiClient(server.accept());
     Serial.println(F("============ New connection ============"));
-    connect_time = millis();
     //String req = client->readStringUntil('\r');
     while (client->available()) {
-      // byte by byte is not very efficient
-      client->read();
+      Serial.print(client->read());
     }
-    //client->print(F("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n"));
+    Serial.println();
   }
   if (client) {
     if (! serveClient() ) {
       Serial.println(F("============ client->stop() ============"));
       client->stop();
       delete(client);
+      status = 0;
       Serial.println(F("============ client = NULL  ============"));
       client = NULL;
     }
-  } else {
-    delay(20);
   }
 }
